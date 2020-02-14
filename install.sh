bf2a9b1c8603e7d058dbd6e440aa3a4b7598b0c1
Jim Hessin <jhessin@gmail.com>
2020-02-14 13:47:49 -0700

    fixed

--- install.sh
+++ install.sh
@@ -32,5 +32,5 @@ fi
 
 localectl set-x11-keymap us pc105 dvp compose:102,numpad:shift3,kpdl:semi,keypad:atm,caps:escape
 localectl set-keymap --no-convert dvp
-setxkbmap -layout us -variant dvp -option compose:102 -option keypad:atm\
+setxkbmap -layout us -variant dvp -option compose:102 -option keypad:atm \
 -option numpad:shift3 -option kpdl:semi -option caps:escape
