f2601927acd99167eb93712fc9434dd932108724
Jim Hessin <jhessin@gmail.com>
2020-02-14 14:47:30 -0700

    repo setup added

--- install.sh
+++ install.sh
@@ -34,6 +34,3 @@ localectl set-x11-keymap us pc105 dvp compose:102,numpad:shift3,kpdl:semi,keypad
 localectl set-keymap --no-convert dvp
 setxkbmap -layout us -variant dvp -option compose:102 -option keypad:atm \
 -option numpad:shift3 -option kpdl:semi -option caps:escape
-
-system_setup/01-preconfig.sh &&
-system_setup/02-install-apps.sh
