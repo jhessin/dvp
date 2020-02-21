622a9c825025d7ac04aeaca13dbdece6fd1dcb08
Jim Hessin <jhessin@gmail.com>
2020-02-21 00:28:19 -0600

    added functions and prompts for confirmation

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+source ./functions.sh
+
+# DISCLAIMER
+echo DO NOT JUST RUN THIS FILE!
+echo this will remove and replace your ~/.local/bin directory.
+echo If you want to save anything from there do that before running this.
+if ! confirm "Are you sure you want to continue"; then
+  exit 1;
+fi
+
 dirs="/usr/share/keymaps/i386/dvorak /usr/share/kbd/keymaps/i386/dvorak"
 for dir in $dirs
 do
@@ -34,3 +44,9 @@ localectl set-x11-keymap us pc105 dvp compose:102,numpad:shift3,kpdl:semi,keypad
 localectl set-keymap --no-convert dvp
 setxkbmap -layout us -variant dvp -option compose:102 -option keypad:atm \
 -option numpad:shift3 -option kpdl:semi -option caps:escape
+
+echo Test your keyboard layout - it should be dvp now
+read
+if confirm "Do you wish to reboot now"; then
+  sudo reboot
+fi
